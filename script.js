f25029a425413b03c9a95d3f7430b13a294f7be2
esther-v <esvalas@gmail.com>
2021-11-13 12:14:16 +0100

    searching for an ipaddress ok

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const apiKey = 'at_yC5cpL6qcU3Gey9kWKcLFMufpoSdW';
+const apiKey = 'at_yC5cpL6qcU3Gey9kWKcLFMufpoSdW'
 
-let ipAddress = '8.8.8.8'
+const apiURL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=`
 
-const apiURL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
+let ipAddress = ''
 
-
+//elements to search for an ip Address
 const searchBtn = document.querySelector('.search')
 const form = document.querySelector('form')
 
+//elements for displaying results 
 const ipAddressDisplay = document.querySelector('.box1 p')
 const cityDisplay = document.querySelector('.box2 .city')
 const countryDisplay = document.querySelector('.box2 .country')
 const timeZoneDisplay = document.querySelector('.box3 p')
 const ispDisplay = document.querySelector('.box4 p')
 
+
+//get data from api, by default it takes the user ip Address
 async function getData() {
-    const response = await fetch(apiURL)
+    const response = await fetch(apiURL+ipAddress)
     const results = await response.json()
     console.log(results)
 
@@ -29,7 +32,13 @@ async function getData() {
 
 getData()
 
-// searchBtn.addEventListener('input', (e)=>{
-//     ipAddress = e.target.value
-//     console.log(ipAddress)
-// })
+//searching for an ipAddress and get results
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchValue = searchBtn.value
+    console.log(searchValue)
+    ipAddress = searchValue
+    getData()
+})
+
