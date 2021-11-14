15c633a1215bf2fd63788d738424277491fb9de0
esther-v <esvalas@gmail.com>
2021-11-14 19:44:48 +0100

    get leaflet map

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const apiURL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAdd
 
 let ipAddress = ''
 
+
+//initialize the map
+let mymap = L.map('map').setView([51.505, -0.09], 13);
+
+//adding tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZXN0ZXZhIiwiYSI6ImNrdnpsMjFlbzA2dmQyd3BxYmszMTBjaWgifQ.B566jerFzMnpk0FIr8Cjcg'
+}).addTo(mymap);
+
+
 //elements to search for an ip Address
 const searchBtn = document.querySelector('.search')
 const form = document.querySelector('form')
